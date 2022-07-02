01d89820322dde762ecec3bc2605d491237e4abd
Megan Reid <meganreid@Megans-iMac.local>
2022-07-02 08:11:31 -0400

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -1,36 +1,45 @@
-# The data we need to retrieve
-# 1. The total number of votes cast
-# 2. A complete list of candidates who received votes
-# 3. The percentage of votes each candidate won
-# 4. The total number of votes each candidate won
-# 5. The winner of the election based on popular vote
-
-# Import the datetime class from the datetime module
-import datetime
-# Use the now() attribute on the datetime class to get the present time
-now = datetime.datetime.now()
-# print the present time now
-print("The time right now is", now)
-
-import datetime as dt
-
-now = dt.datetime.now()
-
-print("The time right now is", now)
-
 import csv
-dir(csv)
+import os
 
-# Assign a variable for the file to load and the path
+file_to_load = os.path.join("Resources", "election_results.csv")
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-file_to_load = 'Resources/election_results.csv'
+total_votes = 0
 
-# Open the election results and read the file
+candidate_options = []
+candidate_votes = {}
 
-with  open(file_to_load) as election_data:
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
-# To do: perform analysis
-        print(election_data)
+with open(file_to_load) as election_data:
+        file_reader = csv.reader(election_data)
+        headers = next(file_reader)
+        for row in file_reader:
+                total_votes += 1
+                candidate_name = row[2]
+                if candidate_name not in candidate_options:
+                        candidate_options.append(candidate_name)
+                        candidate_votes[candidate_name] = 0
 
-# Close the file
+                candidate_votes[candidate_name] += 1
+for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes) / float(total_votes) * 100
+
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+                winning_count = votes
+                winning_percentage = vote_percentage
+                winning_candidate = candidate_name
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+winning_candidate_summary = (
+        f"----------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"----------------------\n")
+        
+print(winning_candidate_summary)
+
 
---
+++ Python_practice.py
@@ -0,0 +1,18 @@
+temperature = int(input("What is the temperature outside?"))
+
+if temperature > 80:
+        print("Turn on the AC")
+
+else:
+        print("Open the windows")
+
+
+temperature = int(input("What is the temperature outside?"))
+
+if temperature > 80:
+        print("Turn on the AC")
+
+else:
+        print("Open the windows")
+
+temperature = "82"
